34aa88ec5b8fac6e03b64b69ebd3b149a25a3669
Tsuman <61953278+MrUnowNSoG@users.noreply.github.com>
2025-06-15 19:16:52 +0300

    [Fix] text

--- Assets/Grift Together/Scripts/RootManager/LocalizationManager/LocalizationText.cs
+++ Assets/Grift Together/Scripts/RootManager/LocalizationManager/LocalizationText.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace GriftTogether {
 
-    [ExecuteInEditMode]
     [DisallowMultipleComponent]
     [RequireComponent(typeof(RectTransform))]
     [RequireComponent(typeof(CanvasRenderer))]
